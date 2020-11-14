cae43810a7851f4f32b49bc44a601c670fb5623f
Anuraag-Chakraborty <71392140+Anuraag-Chakraborty@users.noreply.github.com>
2020-11-14 23:30:40 +0530

    Update Block.js

--- Block.js
+++ Block.js
@@ -4,14 +4,14 @@ class Block{
          
           restitution :0.4,
           friction :1.0,
+          //isStatic:true
+         
       }
-      
       this.body = Bodies.rectangle(x, y, width, height, options);
       this.width = width;
       this.height = height;
       World.add(world, this.body);
     }
-    
     display(){
       var angle = this.body.angle;
       var pos= this.body.position;
